6cbc059e9bb9d81a927a07f86b1d7aabba4a9b82
sakouben <121033140+sakouben@users.noreply.github.com>
2022-12-20 23:41:26 +0700

    Update taghistories.py
    reorganization and repackaging

--- taghistories.py
+++ taghistories.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 import re
 import time
 import pandas as pd
+from datetime import datetime
+
+today_strf_Ymd = pd.Timestamp(datetime.today().strftime('%Y-%m-%d'))
 
 class Tag:
     def __init__(self, url, canon_identifier, pages_of_content="Unloaded", dt_list="Unloaded"):
@@ -80,6 +83,7 @@ class Tag:
         self.pages_of_content = self.ao3_extract_no_pages_of_content(self.url)
         self.dt_list = self.ao3_extract_dt_list(self.url)
         
+        
 def tagdf_init(startdate, enddate=today_strf_Ymd):
     start = pd.Timestamp(startdate)
     end = pd.Timestamp(enddate)
@@ -89,12 +93,36 @@ def tagdf_init(startdate, enddate=today_strf_Ymd):
         
     return super_df
 
+
 def addcolumn(df, Tag): #as Ship object;
     df[Tag.canon_identifier] = 0 #creates empty column
     
-    Tag.load_values()
+    try:
+        for timestamp in Tag.dt_list:
+            df.at[timestamp.strftime("%Y-%m-%d"), Tag.canon_identifier] += 1
+    except:
+        Tag.load_values()
+        for timestamp in Tag.dt_list:
+            df.at[timestamp.strftime("%Y-%m-%d"), Tag.canon_identifier] += 1
             
-    for timestamp in Tag.dt_list:
-        df.at[timestamp, Tag.canon_identifier] += 1
-        
     return df
+
+
+class TagDB:
+    def __init__(self, filename, startdate=None, enddate=None):
+        self.filename = filename
+        self.startdate = startdate
+        self.enddate = enddate
+    
+    def setup_DB(self):
+        df = tagdf_init(self.startdate, self.enddate)
+        df.to_csv(self.filename)
+        
+    def read_DB(self):
+        df = pd.read_csv(self.filename, index_col=0)
+        return df
+    
+    def addtag_DB(self, tag):
+        df = self.read_DB()
+        df = addcolumn(df, tag)
+        df.to_csv(self.filename)
