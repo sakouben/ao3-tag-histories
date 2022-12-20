4bd6e838f14290f0d79ccd175b0f83589a2fb9a1
sakouben <121033140+sakouben@users.noreply.github.com>
2022-12-20 23:51:04 +0700

    Update st_vis.py

--- st_vis.py
+++ st_vis.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-import az0
+import az1
 
-PrimaryDB = az0.TagDB("tag-histories.csv")
+PrimaryDB = az1.TagDB("tag-histories.csv")
 df = PrimaryDB.read_DB()
     
 chart_data = df["Venti/Xiao"]
