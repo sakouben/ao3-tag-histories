d391da44d9c905c95dbeb92f8fe14d1843639a3e
sakouben <121033140+sakouben@users.noreply.github.com>
2022-12-21 01:21:24 +0700

    Update st_vis.py

--- st_vis.py
+++ st_vis.py
@@ -18,27 +18,27 @@ options = st.multiselect(
     
 rolling_avg_days = st.number_input('Days to calculate rolling average', min_value=1)
 
-#startdate = st.date_input(
-#    "x-axis start date",
-#    datetime.date(2019, 7, 6))
-#startd = pd.Timestamp(startdate).strftime("%Y-%m-%d")
-#
-#enddate = st.date_input(
-#    "x-axis end date",
-#    datetime.date(2023, 1, 1))
-#endd = pd.Timestamp(enddate).strftime("%Y-%m-%d")
-#
-#try:
-#    k = df.index.get_loc(startdate)
-#    df = df.iloc[[k, len(df) - 1]]
-#except:
-#    pass
-#
-#try:
-#    k = df.index.get_loc(enddate)
-#    df = df.iloc[[0, k]]
-#except:
-#    pass
+startdate = st.date_input(
+    "x-axis start date",
+    datetime.date(2019, 7, 6))
+startd = pd.Timestamp(startdate).strftime("%Y-%m-%d")
+
+enddate = st.date_input(
+    "x-axis end date",
+    datetime.date(2023, 1, 1))
+endd = pd.Timestamp(enddate).strftime("%Y-%m-%d")
+
+try:
+    k = df.index.get_loc(startd)
+    df = df.iloc[[k, len(df) - 1]]
+except:
+    pass
+
+try:
+    k = df.index.get_loc(endd)
+    df = df.iloc[[0, k]]
+except:
+    pass
 
 dfrm = df[options].rolling(int(rolling_avg_days)).mean()
 dfrm = dfrm.fillna(value=0)
