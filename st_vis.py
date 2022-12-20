af23c543cfe519a087838177d87d6d164c1e84d3
sakouben <121033140+sakouben@users.noreply.github.com>
2022-12-21 00:21:10 +0700

    Update st_vis.py

--- st_vis.py
+++ st_vis.py
@@ -21,3 +21,28 @@ chart_data = dfrm
 
 
 st.line_chart(chart_data)
+
+
+import altair as alt
+from vega_datasets import data
+
+source = data.stocks()
+
+lines = (
+    alt.Chart(source)
+    .mark_line()
+    .encode(x="date", y="price", color="symbol")
+)
+
+xrule = (
+    alt.Chart()
+    .mark_rule(color="cyan", strokeWidth=2)
+    .encode(x=alt.datum(alt.DateTime(year=2006, month="November")))
+)
+
+yrule = (
+    alt.Chart().mark_rule(strokeDash=[12, 6], size=2).encode(y=alt.datum(350))
+)
+
+
+lines + yrule + xrule
