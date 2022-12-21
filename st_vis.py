6fabe4ad2cf8756911bc766a034a1c9d01b07d40
sakouben <121033140+sakouben@users.noreply.github.com>
2022-12-21 10:35:29 +0700

    Update st_vis.py

--- st_vis.py
+++ st_vis.py
@@ -10,7 +10,7 @@ def group_df(df, function="sum", time="month"):
     df['month'] = pd.to_datetime(df['date']).dt.month
     df['year'] = pd.to_datetime(df['date']).dt.year
     df.groupby(['year', 'month'], as_index=False).sum()
-    df.index = pd.DatetimeIndex(zdf.index)
+    df.index = pd.DatetimeIndex(df.index)
     df = df.drop('month', axis=1)
     df = df.drop('year', axis=1)
     
